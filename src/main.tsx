a594b1ce0717e2faf38d4fe30be272e48cc92c12
Devendra2553 <dchauhan2553@gmail.com>
2026-07-21 14:41:18 +0530

    added favicon

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { SplashScreen } from './SplashScreen.tsx';
-const img1 = "/assets/img1.jpg";
-const img2 = "/assets/img2.jpg";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SplashScreen baseLogo={img1} revealLogo={img2} duration={3000} />
+    <SplashScreen duration={3000} />
     <App />
   </StrictMode>,
 );
